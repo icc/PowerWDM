8950f918137365a0e8e974aee0db7ca8b5bf0df4
Frode Petterson <icc@itth.net>
2010-04-14 03:03:35 +0200

    Updated content regex for record, removed reverse order on records and replaced puts with haml_concat

--- app/models/record.rb
+++ app/models/record.rb
@@ -6,7 +6,7 @@ class Record < ActiveRecord::Base
   validates_format_of :name, :with => /^(([a-z0-9]|\*\.){1}[a-z0-9\.\-]*|\*)$/i, :allow_nil => true
   validates_length_of :name, :maximum => 255
   validates_format_of :type, :with => /^(SOA|NS|MX|A|AAAA|CNAME|TXT|PTR|HWINFO|SRV|NAPTR)$/, :allow_nil => true
-  validates_format_of :content, :with => /^[a-z0-9]{1}[a-z0-9\.\-\s]*$/i, :allow_nil => true
+  validates_format_of :content, :with => /^[a-z0-9\.\-\s:_]*$/i, :allow_nil => true
   validates_length_of :content, :maximum => 255
   validates_numericality_of :ttl, :only_integer => true, :allow_nil => true
   validates_numericality_of :prio, :only_integer => true, :allow_nil => true
